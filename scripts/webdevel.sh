29a128cd35261a0926154da40d248b265c7b8af3
Hannes Eichblatt <mail@hanneseichblatt.de>
2014-04-27 09:30:18 +0200

    added webdevel.sh

---
+++ scripts/webdevel.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+source ./vars
+
+# install packages necessary for web development work
+
+$(GROUPINSTALL) "Development Tools"
+$(INSTALL) rubygems ruby-devel rubygem-RedCloth
+$(GEMINSTALL) jekyll
