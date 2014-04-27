a5cae6807e27f2ba387ddbe1fdf5987e4754bb39
Hannes Eichblatt <mail@hanneseichblatt.de>
2014-04-27 22:55:42 +0200

    use cid file

--- test.sh
+++ test.sh
@@ -9,5 +9,5 @@ which docker >&1 >/dev/null
 echo Building image.
 docker build -t "$TAG" .
 echo Running tests in container.
-docker run -i -t "$TAG"
-exit $(docker wait "$TAG")
+docker run -i -t --cidfile=./CID "$TAG"
+exit $(docker wait $(cat CID))
