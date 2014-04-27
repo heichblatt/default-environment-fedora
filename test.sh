4d51c0d21c411d6c312601ce60f2e1bc056e76a7
Hannes Eichblatt <mail@hanneseichblatt.de>
2014-04-27 16:45:29 +0200

    simplified test

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ TAG=heichblatt/test-default-environment-fedora
 
 which docker >&1 >/dev/null
 echo Building image.
-time docker build -t "$TAG" .
+docker build -t "$TAG" .
 echo Running tests in container.
-time ID=$(docker run -i -t "$TAG")
+docker run -i -t "$TAG"
 exit 0
