bb3e5f51eea582a62cca5d3c1049be51cafe0f19
Hannes Eichblatt <mail@hanneseichblatt.de>
2014-04-27 10:12:41 +0200

    more Docker-related settings

--- scripts/devel.sh
+++ scripts/devel.sh
@@ -7,3 +7,6 @@ source ./vars
 
 "$GROUPINSTALL" "Development Tools"
 "$INSTALL" git gitg meld docker-io
+sudo service docker restart
+sudo usermod -aG "$USER" docker
+echo Please log out and back in to apply your group membership in group docker.
